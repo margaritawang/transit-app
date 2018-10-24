8647941ce0c2d202bcc55bbb36958771add11718
margaritawang <amywang.2@alumni.ubc.ca>
2018-10-23 20:58:21 -0700

    added header and footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="footer text-center">
+        Made with love by{' '}
+        <a href="https://margaritawang.github.io">Margarita Amy Wang</a>.
+      </footer>
+    );
+  }
+}
+export default Footer;
---
+++ src/components/header.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Transit App</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="https://www.translink.ca">Translink</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/margaritawang/transit-app">Github</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  <i className="ti-user mr-1 ml-1" /> My Account
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+    )
+  }
+}
+
+export default Header;
