73cf0f5b2832809d4e47600d7bcba09297e3bc18
margaritawang <amywang.2@alumni.ubc.ca>
2018-10-24 21:59:27 -0700

    beautified code

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-
+import React from "react";
 
 class Footer extends React.Component {
   render() {
     return (
       <footer className="footer text-center">
-        Made with <i className="ti-heart mr-1 ml-1"/> by{' '}
-        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/margarita-amy-wang-eit-64223a84/">Margarita Amy Wang</a>.
+        Made with <i className="ti-heart mr-1 ml-1" /> by{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/margarita-amy-wang-eit-64223a84/"
+        >
+          Margarita Amy Wang
+        </a>
+        .
       </footer>
     );
   }
--- src/components/markers.jsx
+++ src/components/markers.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
 
 const Wrapper = styled.div`
   position: absolute;
@@ -13,7 +13,7 @@ const Wrapper = styled.div`
   border-radius: 100%;
   user-select: none;
   transform: translate(-50%, -50%);
-  cursor: ${props => (props.onClick ? 'pointer' : 'default')};
+  cursor: ${props => (props.onClick ? "pointer" : "default")};
   &:hover {
     z-index: 1;
   }
@@ -22,17 +22,17 @@ const Wrapper = styled.div`
 const Marker = props => (
   <Wrapper
     alt={props.text}
-    {...props.onClick ? { onClick: props.onClick } : {}}
+    {...(props.onClick ? { onClick: props.onClick } : {})}
   />
 );
 
 Marker.defaultProps = {
-  onClick: null,
+  onClick: null
 };
 
 Marker.propTypes = {
   onClick: PropTypes.func,
-  text: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired
 };
 
 export default Marker;
